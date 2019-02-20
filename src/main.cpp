efaf676096b8704df5637b1327b6a69e22d89fe8
Juan Diego <jd99@outlook.com>
2019-02-20 15:31:55 -0500

    Se agrega funcionalidad para prevenr fallas, por deficiencias del internet local, se suprime la funcion para variar el periodo de muestreo

--- src/main.cpp
+++ src/main.cpp
@@ -8,52 +8,47 @@
 #include <TimeLib.h>
 
 
+//--------------------------------------------------------------------------------------------------------------------
 
-#define WIFISSID "SEA UCO"                           //credenciales de la red wifi
-#define PASSWORD "SeaUco666"
+
+#define WIFISSID "xxxxxx"                           //credenciales de la red wifi
+#define PASSWORD "xxxxxx"
 
 
 //--------------------------------------------------TANQUE X-----------------------------------------------------------
 
 #define TOKEN  "xxxxxx"        //Token de la cuenta Ubidots
-char* Sample_Time = "xxxxxx";
 char* Time_on_valve = "xxxxxx";                             //Ids de variables Ubidots
 char* lcdb = "xxxxxx";
 char* HourOn = "xxxxxx";
 char* MinuteOn = "xxxxxx";
-
-const int default_hora=6;                                   //Se define la hora de encendido de la válvula
-const int default_minuto=0;
 //---------------------------------------------------------------------------------------------------------------------
+
 
 
 
 #define ONE_WIRE_BUS D3                              //Pin del bus One Wire
 #define GPIO_ADDR   0x27                             //direccion i2c del lcd
-
-
-const int default_sample_time=20;                    //Definicion de constantes por defecto en caso de
-const int default_on_valve_time=0;                   //perdida de conexion wifi. (seg, grados cenigrados)
+#define iteracciones 4
+#define dst  0
+#define timezone -5
 
 
 int valvula = 12;                                    //pin de control de la válvula
-
-
 int Hour=0;
 int Minute=0;
 int Humedad=0;                                       //inicialización de variables
 int Lectura_Analogica=0;
 int reconection_time=0;
-float Sample=0;
 float temp_value=0;
-float On_Time;
+float On_Time=0;
 float valve_state=0;
 float last_valve_state=0;
 float temperatura=0;
 char* Wifistatus="D";
 float lcd_state=0;
-int timezone = -5;
-int dst = 0;
+int it=0;
+
 
 
 LiquidCrystal_I2C lcd(GPIO_ADDR, 20, 4);             //Se inicializa el lcd
@@ -90,7 +85,7 @@ void setup() {
 
  void loop() {
 
-    Lectura_Analogica = analogRead(A0);              //Obtiene la humedad
+   Lectura_Analogica = analogRead(A0);              //Obtiene la humedad
    Humedad = map(Lectura_Analogica,0, 1024, 0, 100);
    Humedad=abs(100-Humedad);
 
@@ -107,19 +102,19 @@ void setup() {
 
 
    time_t now = time(nullptr);                        //Obtiene la hora interna
-   Serial.println(ctime(&now));
 
-   Serial.print("Temperatura: ");                    //Imprime temperatura y humedad puerto serial
-   Serial.println(temperatura);
-   Serial.print("Lectura analógica: ");
-   Serial.println(Lectura_Analogica);
-   Serial.print("Humedad: ");
-   Serial.print(Humedad);
-   Serial.println('%');
-   Serial.print("Hora: ");
-   Serial.print(hour(now));
-   Serial.print(":");
-   Serial.println(minute(now));
+   // Serial.println(ctime(&now));
+   // Serial.print("Temperatura: ");                    //Imprime temperatura y humedad puerto serial
+   // Serial.println(temperatura);
+   // Serial.print("Lectura analógica: ");
+   // Serial.println(Lectura_Analogica);
+   // Serial.print("Humedad: ");
+   // Serial.print(Humedad);
+   // Serial.println('%');
+   // Serial.print("Hora: ");
+   // Serial.print(hour(now));
+   // Serial.print(":");
+   // Serial.println(minute(now));
 
    lcd.setCursor(13,0);                              //Imprime temperatura y humedad en el lcd
    lcd.print(round(temperatura));
@@ -131,33 +126,74 @@ void setup() {
    lcd.print('%');
 
 
-   Sample = client.getValue(Sample_Time);            //Prueba la conectividad con el servidor de Ubidots
+   int value;
 
-   if(Sample==999){                                  //Si NO hay conectividad
-     Serial.println("Error al descargar variables, no esta conectado a internet. ");
-     lcd.setCursor(15,1);
-     lcd.print("D");
-     Sample=default_sample_time*1000;                //Establece los valores POR DEFECTO para tiempo de encendido,
-     On_Time=default_on_valve_time*1000;             //periodo de muestreo y hora de encendido
-     Hour = default_hora;
-     Minute = default_minuto;
-     Serial.println("Se establecieron los valores por defecto");
-     Wifistatus="D";
+   while(it < iteracciones){
+       value =  client.getValue(Time_on_valve);      //Intenta descargar los valores de la nube; de no ser capaz, deja los valores anteriores como estaban
+      if (value != ERROR_VALUE){
+         lcd.setCursor(15,1);
+         lcd.print("C");
+         Wifistatus="C";
+         On_Time=value;
+         On_Time=On_Time*1000;
+         it=0;
+         break;
+      }else{
+         lcd.setCursor(15,1);
+         lcd.print("D");
+         Wifistatus="D";
+      }
    }
 
-   else if(Sample!=999){                             //Si HAY conectividad
-     Serial.println("Esta conectado a internet");
-     lcd.setCursor(15,1);
-     lcd.print("C");
-     On_Time = client.getValue(Time_on_valve);       //Establece los valores de INTERNET para tiempo de encendido,
-     lcd_state = client.getValue(lcdb);
-     Sample=Sample*1000;
-     On_Time=On_Time*1000;
-     Hour = client.getValue(HourOn);
-     Minute = client.getValue(MinuteOn);
-     Serial.println("Se establecieron los valores de la nube");
-     Wifistatus="C";
-     }
+
+   while(it < iteracciones){
+      value =  client.getValue(lcdb);
+       if (value != ERROR_VALUE){
+         lcd_state=value;
+         it=0;
+         break;
+       }
+    }
+
+    while(it < iteracciones){
+       value =  client.getValue(HourOn);
+       if (value != ERROR_VALUE){
+         Hour=value;
+         it=0;
+         break;
+       }
+    }
+
+    while(it < iteracciones){
+       value =  client.getValue(MinuteOn);
+       if (value != ERROR_VALUE){
+         Minute=value;
+         it=0;
+         break;
+       }
+   }
+
+
+
+   // On_Time = client.getValue(Time_on_valve);            //Prueba la conectividad con el servidor de Ubidots
+   //
+   //  if(On_Time!=999){                             //Si HAY conectividad
+   //   Serial.println("Esta conectado a internet");
+   //   lcd.setCursor(15,1);
+   //   lcd.print("C");
+   //   On_Time = client.getValue(Time_on_valve);       //Establece los valores de INTERNET para tiempo de encendido,
+   //   lcd_state = client.getValue(lcdb);
+   //   On_Time=On_Time*1000;
+   //   Hour = client.getValue(HourOn);
+   //   Minute = client.getValue(MinuteOn);
+   //   Wifistatus="C";     // Serial.println("Se establecieron los valores de la nube");
+   //
+   //  }else{                                  //Si NO hay conectividad
+   //   Serial.println("Error al descargar variables, no esta conectado a internet. ");
+   //   lcd.setCursor(15,1);
+   //   lcd.print("D");
+   //  }
+
 
    if(lcd_state==1){
      lcd.backlight();                                //Enciende o apaga la luz del lcd
@@ -190,7 +226,6 @@ void setup() {
     }
 
 
-
    // if(counter>=(On_Time/Sample)){                 //Controla la electrovalvula, si ya ha pasado el tiempo de
    //   counter=0;                                   // encendido o apagado.
    //  if((temperatura>=temp_value) && (last_state==0)){
@@ -208,65 +243,35 @@ void setup() {
    // }
 
 
-
-   if (Wifistatus == "C"){
-     client.add("Humedad", Humedad);                 //Empaqueta los valores de temperatura, humedad, tiempo
-     client.add("Temperatura", temperatura);         //y estado de la válvula
-     client.add("Hora del dispositivo", hour(now));
-     client.add("Minutos del dispositivo", minute(now));
-     client.add("Estado de la valvula", valve_state);
-     client.sendAll(true);                           //Envia el paquete
-     Serial.println("");
-   }
-
-
-   if(Sample>600000){                                //Verifica  que el valor del periodo de muestreo este dentro de
-     Sample=default_sample_time;                     //los limites establecidos
-   }
-
-
-    Serial.print("Wifi State: ");                    //Imprime configuraciones, y lecturas por el puerto serial.
-   Serial.println(Wifistatus);
-   Serial.print("El periodo de muestreo es:  ");
-   Serial.print(round(Sample/1000));
-    Serial.println(" Seg");
-   Serial.print("La valvula permanece encendida por:  ");
-   Serial.print(round(On_Time/1000));
-   Serial.println(" Seg");
-   Serial.print("La temperatura máxima permitida es: ");
-   Serial.print(temp_value);
-   Serial.println(" c");
-   Serial.print("Estado de la valvula: ");
-   Serial.println(round(valve_state));
-   Serial.print("Estado del lcd: ");
-   Serial.println(lcd_state);
-   Serial.print("La hora de encendido es: ");
-   Serial.print(Hour);
-   Serial.print(":");
-   Serial.println(Minute);
-   Serial.println("______________________________________________________________________________________________");
+   client.add("Humedad", Humedad);                 //Empaqueta los valores de temperatura, humedad, tiempo
+   client.add("Temperatura", temperatura);         //y estado de la válvula
+   client.add("Hora del dispositivo", hour(now));
+   client.add("Minutos del dispositivo", minute(now));
+   client.add("Estado de la valvula", valve_state);
+   delay(1000);
+   client.sendAll(true);                           //Envia el paquete
+   Serial.println("");
 
 
 
-   if(Wifistatus=="C"){
-     delay(500);
-     int i = (Sample-2000)/1000;
-     for(i;i>0;i--){
-       delay(1000);                                  //Espera un tiempo antes el iniciar siguiente ciclo, con el
-       lcd.setCursor(13,1);                          //tiempo de la nube
-       lcd.print("  ");
-       lcd.setCursor(12,1);
-       lcd.print(i);
-     }
-   }
-   else{
-     int i = (Sample-10000)/1000;
-     for(i;i>0;i--){
-       delay(1000);                                  //Espera un tiempo antes el iniciar siguiente ciclo, con el
-       lcd.setCursor(13,1);                          //tiempo por defecto
-       lcd.print("  ");
-       lcd.setCursor(12,1);
-       lcd.print(i);
-     }
-   }
+   // Serial.print("Wifi State: ");                    //Imprime configuraciones, y lecturas por el puerto     serial.
+   // Serial.println(Wifistatus);
+   // Serial.print("La valvula permanece encendida por:  ");
+   // Serial.print(round(On_Time/1000));
+   // Serial.println(" Seg");
+   // Serial.print("Estado de la valvula: ");
+   // Serial.println(round(valve_state));
+   // Serial.print("Estado del lcd: ");
+   // Serial.println(lcd_state);
+   // Serial.print("La hora de encendido es: ");
+   // Serial.print(Hour);
+   // Serial.print(":");
+   // Serial.println(Minute);
+   // Serial.println("______________________________________________________________________________________________");
+
+
+
+     delay(10000);                                  //Espera un tiempo antes el iniciar siguiente ciclo
+
+
  }
